377a5b628766d1b9e00e4b2b1a70fb52e150ff08
KirtanTankRed <168075756+KirtanTankRed@users.noreply.github.com>
2024-08-05 14:55:49 +0530

    Update auto_edit.py

--- auto_edit.py
+++ auto_edit.py
@@ -50,7 +50,7 @@ try:
     response.raise_for_status()  # Check if the request was successful
     image_data = BytesIO(response.content)
     sidebar_image = Image.open(image_data)
-    st.sidebar.image(sidebar_image, caption='♦️ RED SOFTWARE', use_column_width=True)
+    st.sidebar.image(sidebar_image, caption='🔶 RED SOFTWARE', use_column_width=True)
 except (requests.exceptions.RequestException, UnidentifiedImageError) as e:
     st.sidebar.error(f"Failed to load image from GitHub: {e}")
 
